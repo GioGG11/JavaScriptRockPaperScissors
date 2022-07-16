6268bcfaf2e55ef7631b26252d5c4a78676efeb1
GioGG11 <gio.garciag11@gmail.com>
2022-07-15 23:22:08 -0500

    ugly but works, still needs match

--- script.js
+++ script.js
@@ -3,36 +3,54 @@ function randomChoice(max) {
     return rand;
   }
   var result;
+
 //0 will be scissors, 1 will be rock, 2 will be paper
-  function randomChoice0(){
+  function test(){
+    alert("Test");
+  }
+
+function randomChoice0(){
     var comp = randomChoice(3);
     if (comp == 0){
         result = "tie";
+        console.log(result);
+        alert(result);
     }
     else if (comp == 1){
         result = "lost";
+        console.log(result);
+        alert(result);
     }
-    else{ result = "win"}
+    else{ result = "win"
+    console.log(result);
+    alert(result);
+  }
   }
 
   function randomChoice1(){
     var comp = randomChoice(3);
     if (comp == 0){
         result = "win";
+        alert(result);
     }
     else if (comp == 1){
         result = "tie";
+        alert(result);
     }
-    else{ result = "lost"}
+    else{ result = "lost"
+    alert(result);}
   }
 
   function randomChoice2(){
     var comp = randomChoice(3);
     if (comp == 0){
         result = "lost";
+        alert(result);
     }
     else if (comp == 1){
         result = "win";
+        alert(result);
     }
-    else{ result = "tie"}
+    else{ result = "tie";
+    alert(result);}
   }
